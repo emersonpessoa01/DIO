da23a51852795dfa71fd5f8d3ec208c1ceb1405a
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-11-19 18:04:25 -0300

    added pokeApi :sparkles:

--- 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
+++ 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
@@ -1,7 +1,3 @@
-const offeset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offeset=${0}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
@@ -20,9 +16,8 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
+pokeApi
+  .getPokemons()
   .then((pokemons) => {
     for (let i = 0; i < pokemons.length; i++) {
       // console.log(pokemons[i])
