15927cb764901343d55c39dde9e20d1340728eaa
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-11-19 15:15:09 -0300

    added pokemons :sparkles:

--- 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
+++ 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
@@ -1,20 +1,33 @@
-// https://pokeapi.co/api/v2/pokemon/ditto;
-
 const offeset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offeset=${0}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+  <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name"> ${pokemon.name}</span>
+    <div class="detail">
+      <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+      </ol>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg " alt=${pokemon.name}>
+    </div>
+  </li>
+  `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
 fetch(url)
-  .then((response) =>
-    //return console.log(response)
-    response.json()
-  )
-  //promessa de um json dentro body
-  //requisição com promessa de uma resposta convertida em json
-
-  .then((jsonBody) => console.log(jsonBody))
-  //json body convertido
-  //imprimir o json
-
-  .catch((error) => console.error(error))
-  .finally(() => console.log("Requisição concluída!"));
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      // console.log(pokemons[i])
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
+  })
+  .catch((error) => console.error(error));
