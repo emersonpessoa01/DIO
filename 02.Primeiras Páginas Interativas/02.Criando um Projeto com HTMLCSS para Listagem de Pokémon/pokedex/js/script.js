dc9af99221532267f2101231cd20b2cd17b1cccd
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-11-20 11:57:52 -0300

    added map :sparkles:

--- 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
+++ 02.Primeiras Páginas Interativas/02.Criando um Projeto com HTMLCSS para Listagem de Pokémon/pokedex/js/script.js
@@ -1,3 +1,15 @@
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi
+  .getPokemons()
+  .then((pokemons = []) => {
+    /* array method: map, filter, some, every, reduce, */
+    const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon));
+    // console.log(newList);
+    pokemonList.innerHTML += newList.join(" ");
+  })
+  .catch((error) => console.error(error));
+
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
@@ -8,24 +20,8 @@ function convertPokemonToLi(pokemon) {
         <li class="type">grass</li>
         <li class="type">poison</li>
       </ol>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg " alt=${pokemon.name}>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg " alt=${pokemon.name}>
     </div>
   </li>
   `;
 }
-
-const pokemonList = document.getElementById("pokemonList");
-
-pokeApi
-  .getPokemons()
-  .then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-      // console.log(pokemons[i])
-      const listItems =[]
-      const pokemon = pokemons[i];
-      listItems.push(convertPokemonToLi(pokemon))
-      // pokemonList.innerHTML += convertPokemonToLi(pokemon);
-      pokemonList.innerHTML += listItems;
-    }
-  })
-  .catch((error) => console.error(error));
